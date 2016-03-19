286b292433dfa23bb2e093536a9fc1b029f695d0
Bart Vrancken <bart@abuse.io>
2016-03-19 20:39:03 +0100

    remove unused uri value

--- src/Spamcop.php
+++ src/Spamcop.php
@@ -84,7 +84,6 @@ class Spamcop extends Parser
                     $incident->source_id   = false;
                     $incident->ip          = $report['Source-IP'];
                     $incident->domain      = empty($url) ? false : getDomain($url);
-                    $incident->uri         = empty($url) ? false : getUri($url);
                     $incident->class       = config("{$this->configBase}.feeds.{$this->feedName}.class");
                     $incident->type        = config("{$this->configBase}.feeds.{$this->feedName}.type");
                     $incident->timestamp   = strtotime($report['Received-Date']);
